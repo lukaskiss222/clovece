b5b8ad921466b14af4473796bf5c71562d524d17
lukaskiss222 <lukaskiss222@gmail.com>
2017-04-03 22:01:42 +0200

    Fungujuce clovece

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ class State(object):
             self.pl = [(0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)]
         self.turn = turn
     
-    def dice(self, player_id, thing_id):
-        step = random.randint(1, 6)
-        print("Dice: ", step)
+    def dice(self, player_id, thing_id, step):
         player = list(self.pl[player_id])
         player[thing_id] += step
         copy_pl = copy.deepcopy(self.pl)
@@ -23,16 +21,33 @@ class State(object):
         return State(copy_pl, new_turn)
     
     def print_state(self):
-        for i in range(0, len(self.players)):
-            print("Player %d's pieces are on fields:%d:40 %d:41 %d:42 %d:43" 
-                    % (i, self.players[i][0], self.players[i][1],
-                        self.players[i][2],
-                        self.players[i][3]))
+        for i in range(0, len(self.pl)):
+            print("Player %d's pieces are on fields: 0:%d 1:%d 2:%d 3:%d" 
+                    % (i, self.pl[i][0], self.pl[i][1],
+                        self.pl[i][2],
+                        self.pl[i][3]))
 
+    def turn_player(self):
+        return self.turn % 4
+    def pass_turn(self):
+        return State(self.pl, self.turn +1)
 
 if __name__ == "__main__":
     reference = State()
-    
+    while(reference.turn_player() < 10):
+        print("Player %d on turn." % reference.turn_player())
+        dice = random.randint(1,6)
+        print("Dice: ",dice)
+        reference.print_state()
+        command = input(">")
+        if(command.startswith("move ")):
+            id_thing = command.split(" ")[1]
+            reference = reference.dice(
+                    reference.turn_player(),
+                    int(id_thing),dice)
+        else:
+            reference = reference.pass_turn() 
+        
 
 
 
